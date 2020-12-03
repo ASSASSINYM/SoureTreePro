a9c7e42e98724317e0942f0d0517a7823e5574eb
ASSASSINYM <darlingiloveu@sina.com>
2020-12-04 00:04:39 +0800

    我自己先提交本地的所有变更到本地仓库

--- GitOperationDemo/GitOperationDemo/Sheep.m
+++ GitOperationDemo/GitOperationDemo/Sheep.m
@@ -9,4 +9,13 @@
 
 @implementation Sheep
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        //我在本地做了 改动，我不想保存本地的，我想保存远程的
+    }
+    return self;
+}
+
 @end
--- GitOperationDemo/GitOperationDemo/Student.m
+++ GitOperationDemo/GitOperationDemo/Student.m
@@ -8,5 +8,13 @@
 #import "Student.h"
 
 @implementation Student
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        // 我在本地做了改动，我想保存本地的
+    }
+    return self;
+}
 
 @end
